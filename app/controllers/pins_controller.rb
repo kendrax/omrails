2957b8fcfac855efca91229b09f38b7898325110
Kendra Clarke <kendra@clarkwithane.com>
2013-09-03 13:42:35 -0400

    Add Paperclip Gem

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -79,6 +79,10 @@ class PinsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to pins_url }
       format.json { head :no_content }
+  def pin_params
+    params.require(:pin).permit(:description, :image)
+  end
+
     end
   end
 end
